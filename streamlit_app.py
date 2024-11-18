ce8d1e17568df7d058f2f971a1cc03b5f79e02b3
yeenaaaaa24 <20242915@sdhs.gwe.hs.kr>
2024-11-18 15:03:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 #이전 수업 시간에 만들었던 이미지 분류 pkl 파일을 바탕으로 한 이미지 분류 모델을 Streamlit에 올리는 예제 코드
 #파일 이름 streamlit_app.py
 
-import ,
+import streamlit
 from fastai.vision.all import *
 from PIL import Image
 import gdown
