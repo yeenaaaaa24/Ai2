0384c8f3edb564bb4b42a02c47b423fd58e02ace
yeenaaaaa24 <20242915@sdhs.gwe.hs.kr>
2024-11-18 15:11:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if uploaded_file is not None:
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
     if prediction == labels[0]:
-         st.write("중냉 꿋굿")
+         st.write("중냉")
     elif prediction == labels[1]:
          st.write("짜장면은 굿")
     elif prediction == labels[2]:
